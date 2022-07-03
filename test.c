9f2993ddfee1cd87c82876e9eb032a8f329b9964
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2022-07-03 19:16:06 +0530

    main

--- test.c
+++ test.c
@@ -2,21 +2,29 @@
 #include<stdio.h>
 
 int main(){
-    struct key_value
-    {
-       int key;
-       char* value;
-    };
+    // struct key_value
+    // {
+    //    int key;
+    //    char* value;
+    // };
 
-    struct key_value kv;
+    // struct key_value kv;
 
-    kv.key = 1;
-    kv.value = "foo";
+    // kv.key = 1;
+    // kv.value = "foo";
 
-    kv.key = 2;
-    kv.value = "HELLO";
+    // kv.key = 2;
+    // kv.value = "HELLO";
 
-    printf("%i", kv.key);
-    printf("%s", kv.value);
+    // printf("%i", kv.key);
+    // printf("%s", kv.value);
+    char string[] = "Hello";
+    printf("%c\n", string[0]);
+    printf("%c\n", string[1]);
+    printf("%c\n", string[2]);
+    printf("%c\n", string[3]);
+    printf("%c\n", string[4]);
+    printf("%c\n", string[5]);
+    printf("%c\n", string[6]);
     return 0;
 }
